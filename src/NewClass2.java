e96b77b28833938985e28f69f51e7da27137f2b2
Joshua <98660305+JoshuaOsin@users.noreply.github.com>
2022-04-09 15:38:17 -0400

    New Class

--- src/NewClass2.java
+++ src/NewClass2.java
@@ -1,5 +1,16 @@
 public class NewClass2 {
 
+        String musicInsturment;
 
+    public NewClass2(String musicInsturment) {
+        this.musicInsturment = musicInsturment;
+    }
 
+    public String getMusicInsturment() {
+        return musicInsturment;
+    }
+
+    public void setMusicInsturment(String musicInsturment) {
+        this.musicInsturment = musicInsturment;
+    }
 }
